25c1a53784f3cc591633358617f0b3c692ea82ce
Tara Ellicott <taraellicott@gmail.com>
2018-04-16 20:58:38 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,38 @@
-# your code goes here
+ def begins_with_r
+ end
+    Return true if every element of the tools array starts with an "r" and false otherwise. (FAILED - 1)
+    should return false if there's an element that does not begin with r (FAILED - 2)
+  
+  
+  def contain_a
+end
+    return all elements that contain the letter 'a' (FAILED - 3)
+  
+  
+  def first_wa
+end
+    Return the first element that begins with the letters 'wa' (FAILED - 4)
+ 
+ 
+  def remove_non_strings
+end
+    remove anything that's not a string from an array (FAILED - 5)
+  
+  
+def count_elements
+end
+    count how many times something appears in an array (FAILED - 6)
+  
+  
+def merge_data
+end
+    combines two nested data structures into one (FAILED - 7)
+  
+  
+def find_cool
+end
+    find all cool hashes (FAILED - 8)
+  
+  
+def organize_schools
+end
