f9ec1d5e6ab99c0ab4da53decdcc718b1df929bd
Tara Ellicott <taraellicott@gmail.com>
2018-04-16 21:07:59 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,36 +7,36 @@ def begins_with_r(arr)
 end
 
   
-def contain_a
-end
-    return all elements that contain the letter 'a' (FAILED - 3)
+# def contain_a
+# end
+#     return all elements that contain the letter 'a' (FAILED - 3)
   
   
-  def first_wa
-end
-    Return the first element that begins with the letters 'wa' (FAILED - 4)
+#   def first_wa
+# end
+#     Return the first element that begins with the letters 'wa' (FAILED - 4)
  
  
-  def remove_non_strings
-end
-    remove anything thats not a string from an array (FAILED - 5)
+#   def remove_non_strings
+# end
+#     remove anything thats not a string from an array (FAILED - 5)
   
   
-def count_elements
-end
-    count how many times something appears in an array (FAILED - 6)
+# def count_elements
+# end
+#     count how many times something appears in an array (FAILED - 6)
   
   
-def merge_data
-end
-    combines two nested data structures into one (FAILED - 7)
+# def merge_data
+# end
+#     combines two nested data structures into one (FAILED - 7)
   
   
-def find_cool
-end
-    find all cool hashes (FAILED - 8)
+# def find_cool
+# end
+#     find all cool hashes (FAILED - 8)
   
   
-def organize_schools
-end
-  organizes the schools by location (FAILED - 9)
+# def organize_schools
+# end
+#   organizes the schools by location (FAILED - 9)
